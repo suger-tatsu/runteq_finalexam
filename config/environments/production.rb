a1980c335d33945a1e1e0a1491f3a2e94b541a99
Sato <sato1214@example.com>
2025-05-26 18:27:25 +0900

    卒業試験guru-puwake10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,8 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.require_master_key = false
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
