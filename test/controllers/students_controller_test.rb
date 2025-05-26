b0a57bb0998efeee598784ff988c089d6ff55996
Sato <sato1214@example.com>
2025-05-26 14:41:50 +0900

    卒業試験guru-puwake9

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class StudentsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @teacher = teachers(:one) # fixtures/teachers.yml の one を使う
+    post login_url, params: { email: @teacher.email, password: "password123" }
+  end
+
   test "should get index" do
-    get students_index_url
+    get students_url
     assert_response :success
   end
 end
