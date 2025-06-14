a086d587d89b82c1fba6a3478620709179249a1e
Sato <sato1214@example.com>
2025-06-15 08:44:39 +0900

    卒業試験guru-puwake18

--- test/controllers/group_assignments_controller_test.rb
+++ test/controllers/group_assignments_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class GroupAssignmentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @teacher = teachers(:one)
+    @student = Student.create!(
+      name: "テスト生徒",
+      athletic_ability: 5,
+    leadership: 4,
+    cooperation: 3,
+    science: 2,
+    humanities: 1,
+    teacher: @teacher
+  )
+
+  @assignment = GroupAssignment.new(
+    title: "テスト用課題",
+    teacher: @teacher,
+    public_token: "testtoken",
+    public_enabled: true,
+    group_count: 1,
+    ability_selection: [ "athletic_ability" ],
+    strategy: "even"
+  )
+
+  @assignment.selected_student_ids = [ @student.id ]
+  @assignment.ability_weights = { "athletic_ability" => 1 }
+  @assignment.public_password = "sample"
+  @assignment.save_and_assign_groups
+end
 end
