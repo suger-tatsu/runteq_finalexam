394dadc5db020ae63b8e7661c270af02f26773b6
Sato <sato1214@example.com>
2025-06-29 04:51:22 +0900

    生徒セキュリティlint修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,10 +13,10 @@ module ApplicationHelper
     direction = current_sort == "#{column}_asc" ? "desc" : "asc"
 
     icon = case current_sort
-           when "#{column}_asc" then "▲"
-           when "#{column}_desc" then "▼"
-           else "⇅"  # ソート未選択時も分かりやすく
-           end
+    when "#{column}_asc" then "▲"
+    when "#{column}_desc" then "▼"
+    else "⇅"  # ソート未選択時も分かりやすく
+    end
 
     link_to "#{title} #{icon}".html_safe,
             request.query_parameters.merge(sort: "#{column}_#{direction}", page: nil),
